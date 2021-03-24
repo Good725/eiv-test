10756fe0bc4a9a129853b1461178236d366c7c2a
good725 <firly.larly0430@gmail.com>
2021-03-24 14:42:33 -0700

    test

---
+++ src/helpers/stableCoin/constants.jsx
@@ -0,0 +1,12 @@
+export const ADDR_USDC_VAULT = "0x7cec239505f9c1f3b439237365b3c36f8c25fdac";
+
+export const STABLE_COIN = [
+  {
+    title: "USDC",
+    tokenName: "USDC",
+    iconName: "fUSDC.png",
+    address: "0x7cec239505f9c1f3b439237365b3c36f8c25fdac",
+    token0: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    token1: "0xD7B7d3C0bdA57723Fb54ab95Fd8F9EA033AF37f2",
+  }
+];
